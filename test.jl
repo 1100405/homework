dda11dcf31a353c3d26112365505fbed1a99712c
Brandon <100693893+1100405@users.noreply.github.com>
2024-03-19 14:20:56 +0800

    Delete test.jl

--- test.jl
+++
@@ -1,9 +0,0 @@
-groups = Dict(1 => [], 2 => [], 3 => [], 4 => [])
-compare = [2, 5, 8, 0, 4, 1]
-tmp = minimum(compare)
-t = argmin(compare)
-i = (2, 1)
-j = (2, 1)
-push!(groups[argmin(compare)], i)
-push!(groups[argmin(compare)], j)
-println(groups)
